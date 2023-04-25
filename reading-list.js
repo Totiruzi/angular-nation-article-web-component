1c5b2b39c2436d53aa6243741b096fb9fe3d4f47
Totiruzi <totiruzi@yahoo.com>
2023-04-25 19:52:38 +0300

    clean up comments

--- reading-list.js
+++ reading-list.js
@@ -5,32 +5,3 @@ class ReadingListElement  extends HTMLElement {
   }
 }
 customElements.define('reading-list-element', ReadingListElement);
-
-
-
-// class ReadingListElement  extends HTMLElement {
-//   constructor() {
-//     super();
-//     this.attachShadow({ mode: 'open' });
-//   }
-
-//   connectedCallback() {
-//     this.render();
-//   }
-
-//   render() {
-//     this.shadowRoot.innerHTML = `
-//       <style>
-//         :host {
-//           display: block;
-//           border: 1px solid #ccc;
-//           border-radius: 4px;
-//           padding: 8px;
-//           margin-bottom: 8px;
-//         }
-//       </style>
-//       <slot></slot>
-//     `;
-//   }
-
-// }
